bb5bd8d629625e9b5bc3a711f345dee79a1c2cb1
Josh Forisha <josh@forisha.com>
2014-09-07 02:14:28 -0500

    Add fixture creation and improve progress messages

--- .quarry/server.js
+++ .quarry/server.js
@@ -9,8 +9,10 @@ var util = require('util');
 var blue   = '\033[0;34m';
 var green  = '\033[0;32m';
 var normal = '\033[0;m';
+var purple = '\033[0;35m';
 var red    = '\033[0;31m';
 var white  = '\033[1;37m';
+var yellow = '\033[0;33m';
 
 // App-specific variables
 var config = require('../config.json');
@@ -26,86 +28,25 @@ connectionPreString = 'postgres://' + config.database.user +
   (config.database.password ? ':' + config.database.password : '') +
   '@' + config.database.host + ':' + config.database.port;
 
-function createFixtures(database) {
-  if (!database.config.hasOwnProperty('fixtures') || database.config.fixtures.length < 1) {
-    return;
-  }
-
-  util.print('Connecting to ' + white + database.name + normal + '... ');
-  pg.connect(database.connectionString, function (error, client, done) {
-    if (error) {
-      return logError(error);
-    }
-    logSuccess('Okay');
-
-    for (var tableName in database.config.fixtures) {
-      if (database.config.fixtures.hasOwnProperty(tableName)) {
-        database.config.fixtures[tableName].forEach(function (fixture) {
-          var columnNames = database.config.fixtures[tableName];
-          var columns = [];
-          var values = [];
-          var valueVars = [];
-
-          query = 'INSERT INTO ' + tableName + ' (';
-          for (var columnName in fixture) {
-            if (fixture.hasOwnProperty(columnName)) {
-              columns.push(columnName);
-              values.push(fixture[columnName]);
-              valueVars.push('$' + (valueVars.length + 1));
-            }
-          }
-
-          query += columns.join(', ') + ') VALUES (' + valueVars.join(', ') + ')';
-          console.log(query);
-          /*
-          client.query(query, values, function (error, result) {
-            if (error) {
-              done();
-              logError('Error inserting fixture: $1', error);
-            }
-          });
-          // */
-        });
-      }
-    }
-
-    done();
-
-    /*
-    client.on('drain', function () {
-      logSuccess('Inserted $1 fixtures', database.name);
-      done();
-    });
-    // */
-  });
-}
-
 function createTables(database) {
   if (!database.config.hasOwnProperty('tables') || database.config.tables.length < 1) {
-    return;
+    return logNotice('No tables found for database $1', database.name);
   }
 
-  util.print('Connecting to ' + white + database.name + normal + '... ');
   pg.connect(database.connectionString, function (error, client, done) {
     if (error) {
-      return logError(error);
+      logError('Error connecting to database $1', database.name);
+      return console.log(error.toString());
     }
-    logSuccess('Okay');
 
-    var columns;
-    var columnName;
-    var columnNames;
-    var query;
-    var tableName;
-    var type;
-
-    for (tableName in database.config.tables) {
+    for (var tableName in database.config.tables) {
       if (database.config.tables.hasOwnProperty(tableName)) {
-        columnNames = database.config.tables[tableName];
-        columns = [];
-        query = 'CREATE TABLE ' + tableName + ' (';
+        var columnNames = database.config.tables[tableName];
+        var columns = [];
+        var query = 'CREATE TABLE ' + tableName + ' (';
+        var type;
 
-        for (columnName in columnNames) {
+        for (var columnName in columnNames) {
           if (columnNames.hasOwnProperty(columnName)) {
             type = columnNames[columnName];
             columns.push(columnName + ' ' + type);
@@ -114,33 +55,70 @@ function createTables(database) {
 
         query += columns.join(', ') + ')';
 
-        util.print(
-          'Creating table ' + white + tableName + normal +
-          ' in database ' + white + database.name + normal + '... '
-        );
-        client.query(query, function (error, result) {
-          if (error) {
-            logError(error);
-          }
+        (function (_tableName) {
+          client.query(query, function (error, result) {
+            if (error) {
+              logError('Error creating table $1 in database $2', _tableName, database.name);
+              return console.log(error.toString());
+            }
 
-          logSuccess('Okay');
-        });
+            logSuccess('Created table $1 in database $2', _tableName, database.name);
+
+            if (!database.config.hasOwnProperty('fixtures') || !database.config.fixtures.hasOwnProperty(_tableName)) {
+              return logNotice('No $1 fixture data found for database $2', _tableName, database.name);
+            }
+
+            logInfo('Inserting $1 fixtures in database $2', _tableName, database.name);
+
+            database.config.fixtures[_tableName].forEach(function (fixture) {
+              var values = [];
+              var valueVars = [];
+
+              columns = [];
+
+              query = 'INSERT INTO ' + _tableName + ' (';
+              for (columnName in fixture) {
+                if (fixture.hasOwnProperty(columnName)) {
+                  columns.push(columnName);
+                  values.push(fixture[columnName]);
+                  valueVars.push('$' + (valueVars.length + 1));
+                }
+              }
+
+              query += columns.join(', ') + ') VALUES (' + valueVars.join(', ') + ')';
+
+              client.query(query, values, function (error, result) {
+                if (error) {
+                  logError('Error inserting fixture');
+                  return console.log(error.toString());
+                }
+              });
+            });
+          });
+        })(tableName);
       }
     }
 
     client.on('drain', function () {
+      logSuccess('Finished adding tables and fixtures');
       done();
-      createFixtures(database);
     });
   });
 }
 
-function logError(message) { console.log(red + message.toString() + normal); }
-function logNotice(message) { console.log(blue + message.toString() + normal); }
-function logSuccess(message) { console.log(green + message.toString() + normal); }
+function log(color, args) {
+  console.log(color + args[0].replace(/\$([0-9]*)/g, function (match, value) {
+    return white + args[value].toString() + color;
+  }) + normal);
+}
+
+function logError()   { log(red,    arguments || []); }
+function logInfo()    { log(purple, arguments || []); }
+function logNotice()  { log(blue,   arguments || []); }
+function logSuccess() { log(green,  arguments || []); }
+function logWarning() { log(yellow, arguments || []); }
 
 function startServer() {
-  // Start HTTP server and listening service
   http.createServer(function (request, response) {
     var databaseName;
     var recordId;
@@ -181,7 +159,7 @@ function startServer() {
     pg.connect(database.connectionString, function (error, client, done) {
       if (error) {
         response.writeHead(500, headers);
-        return response.end({ error: 'Error connecting to database: ' + error});
+        return response.end({ error: 'Error connecting to database: ' + error });
       }
 
       client.query(query, params, function (error, result) {
@@ -213,34 +191,33 @@ fs.readdirSync( 'databases' ).forEach(function (databaseConfigScript) {
 
   database.connectionString = connectionPreString + '/' + database.name;
 
-  util.print('Connecting to Postgres... ');
   pg.connect(connectionPreString, function (error, client, done) {
     if (error) {
-      return logError(error);
+      logError('Error connecting to Postgres');
+      return console.log(error.toString());
     }
-    logSuccess('Okay');
 
     // Check if the database exists
-    util.print('Checking database ' + white + database.name + normal + '... ');
     client.query('SELECT 1 FROM pg_database WHERE datname = $1', [ database.name ], function (error, result) {
       if (error) {
-        return logError(error);
+        logError('Error connecting to database $1', database.name);
+        return console.log(error.toString());
       }
 
       if (result.rowCount > 0) {
         done();
-        return logNotice('Found');
+        return logNotice('Found database $1', database.name);
       }
 
       // Create the database if it doesn't exist
-      util.print('Creating... ');
       var owner = database.config.owner || config.database.user;
       client.query('CREATE DATABASE ' + database.name + ' OWNER = ' + owner, function (error, result) {
         if (error) {
-          return logError(error);
+          logError('Error creating database $1', database.name);
+          return console.log(error.toString);
         }
 
-        logSuccess('Okay');
+        logSuccess('Created database $1', database.name);
         done();
 
         createTables(database);
